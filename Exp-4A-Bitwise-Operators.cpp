ff42296ff4b402f7b27a1118c7d488733c2d8e17
Rajendra Phani Vithal Chaganti <rajendra2006ch@gmail.com>
2025-07-20 21:35:28 +0530

    Exp-4A-Bitwise-Operators.cpp

---
+++ Exp-4A-Bitwise-Operators.cpp
@@ -0,0 +1,32 @@
+// NAME : Rajendra Chaganti
+// PRN : 24070123150
+// Experiment 4: Bitwise Operators in C++
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    // Taking input from user
+    cout << "Enter first number a: ";
+    cin >> a;
+    cout << "Enter second number b: ";
+    cin >> b;
+    // Bitwise AND
+    int andResult = a & b;
+    cout << "\na & b = " << andResult << " (Bitwise AND)";
+    // Bitwise OR
+    int orResult = a | b;
+    cout << "\na | b = " << orResult << " (Bitwise OR)";
+    // Bitwise XOR
+    int xorResult = a ^ b;
+    cout << "\na ^ b = " << xorResult << " (Bitwise XOR)";
+    // Bitwise NOT of a
+    int notResult = ~a;
+    cout << "\n~a = " << notResult << " (Bitwise NOT of a)";
+    // Left Shift
+    int leftShift = a << 1;
+    cout << "\na << 1 = " << leftShift << " (Left Shift by 1 bit)";
+    // Right Shift
+    int rightShift = a >> 1;
+    cout << "\na >> 1 = " << rightShift << " (Right Shift by 1 bit)";
+    return 0;
+}
